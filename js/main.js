35029e5f2f9156500ff232223951078c06a836e9
zmx2321@163.com <zmx2321@163.com>
2016-09-21 21:03:01 +0800

    个人简历

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ console.log('This would be the main JS file.');
         this.each(function(){
 			$(this).find('li').bind(options.eventType1,function(){
 				$(this).addClass('nav_on').siblings().removeClass('nav_on');
+				$('article section').hide();
+				id=$(this).attr('id');
+				$("."+id+"_box").show();
 			});
 			$(this).find('li:not(.introduce)').bind(options.eventType1,function(){
 			    console.log($(this).find('li:not(.introduce)'));
