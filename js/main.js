155c74f9f6d4c137639e804690b2cbc62d9b3d63
zmx2321@163.com <zmx2321@163.com>
2016-09-21 20:26:17 +0800

    个人简历首页

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log('This would be the main JS file.');
+/* navList s */
+;(function( $ ){
+    $.fn.navList = function(options){
+        var defaults = {
+            eventType1:'click',
+        }
+
+        var options = $.extend(defaults,options);
+
+        this.each(function(){
+			$(this).find('li').bind(options.eventType1,function(){
+				$(this).addClass('nav_on').siblings().removeClass('nav_on');
+			});
+			$(this).find('li:not(.introduce)').bind(options.eventType1,function(){
+			    console.log($(this).find('li:not(.introduce)'));
+			});
+			$(this).find('li.introduce').bind(options.eventType1,function(){
+			    console.log($(this).find('li.introduce'));
+			});
+        });
+    }
+})(jQuery);
+
+$(function(){
+    $('nav ul').navList();
+});
+
+/* navList e */
